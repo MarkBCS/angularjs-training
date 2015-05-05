c0fc5ab0a13de2ae494acdc16b68cd7347e6f5b4
Lauri Hahne <lauri.hahne@gofore.com>
2015-05-05 12:28:35 +0300

    add main module

--- examples/fullstack/src/js/main.js
+++ examples/fullstack/src/js/main.js
@@ -1,8 +1,9 @@
 let $       = require('jquery')
 let angular = require('angular')
 
+let app = angular.module('app', [])
 
 $(() => {
   let root = $('#root')
-  angular.bootstrap(root)
+  angular.bootstrap(root, ['app'])
 })
