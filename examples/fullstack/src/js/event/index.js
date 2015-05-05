f37a8994de07b514964c182c3e525d4d99847684
Lauri Hahne <lauri.hahne@gofore.com>
2015-05-05 13:07:34 +0300

    add template & jade transform

--- examples/fullstack/src/js/event/index.js
+++ examples/fullstack/src/js/event/index.js
@@ -1,7 +1,9 @@
 angular
   .module('event', [require('angular-route')])
   .config(['$routeProvider', ($routeProvider) => {
-
+    $routeProvider.when('/', {
+      template: require('./templates/index.jade')
+    })
   }])
 
 module.exports = 'event'
