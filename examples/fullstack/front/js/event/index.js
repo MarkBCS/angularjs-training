2f7193ed5ab3c7fbecf719957dbf966ad8ec6cec
Lauri Hahne <lauri.hahne@gofore.com>
2015-05-12 13:54:44 +0300

    mar space

--- examples/fullstack/front/js/event/index.js
+++ examples/fullstack/front/js/event/index.js
@@ -1,10 +1,13 @@
 angular
   .module('event', [require('angular-route')])
+
   .directive('participants', require('./directives/participants'))
+
   .factory('participantService', require('./participantService'))
+
   .config(['$routeProvider', ($routeProvider) => {
     $routeProvider.when('/', {
-      template: require('./templates/index.jade')
+      template: require('./templates/index.html')
     })
   }])
 
