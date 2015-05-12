5a267f4b457ad98470ea25c88858a4b471f9fedc
Lauri Hahne <lauri.hahne@gofore.com>
2015-05-12 13:58:38 +0300

    require rest of jade from angular side

--- examples/fullstack/front/js/event/directives/participants.js
+++ examples/fullstack/front/js/event/directives/participants.js
@@ -1,7 +1,7 @@
 module.exports = () => ({
     restrict: 'E',
     scope: {},
-    template: require('../templates/participants.jade'),
+    template: require('../templates/participants.html'),
     controller: ['$scope', '$filter', 'participantService', ($scope, $filter, participantService) => {
       participantService.participants.success((participants) => {
         $scope.participants = $filter('orderBy')(participants, 'name')
