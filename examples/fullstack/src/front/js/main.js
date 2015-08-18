072a2a5974bc2e1b522c4efbc4d9ab1e827aac3c
jsalonen <jaakko.salonen@iki.fi>
2015-08-18 20:39:34 +0300

    Remove unused examples

--- examples/fullstack/src/front/js/main.js
+++
@@ -1,9 +0,0 @@
-let $       = require('jquery')
-let angular = require('angular')
-
-let app = angular.module('exampleApp', [require('./event')])
-
-$(() => {
-  let root = $('#root')
-  angular.bootstrap(root, ['exampleApp'])
-})
--- examples/fullstack/test/protractor/example_test.js
+++
@@ -1,7 +0,0 @@
-describe('Protractor Demo App', () => {
-  it('should have a title', () => {
-    browser.get('/');
-
-    expect(browser.getTitle()).toEqual('Angular template');
-  });
-});
