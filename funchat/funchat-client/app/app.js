4065496a3986b657b5857f7abedc14caa30bd5d1
Marko Rautajoki <marko.rautajoki@gofore.com>
2015-08-18 21:17:55 +0300

    removed directive from funchat

--- funchat/funchat-client/app/app.js
+++ funchat/funchat-client/app/app.js
@@ -2,5 +2,18 @@
 
 angular.module('funchat', [])
 
-  .controller('appController', function($scope) {
-  });
+  .controller('appController', function($scope, FunchatApi) {
+
+    function listMessages() {
+      FunchatApi.list().then(function(messages) {
+        $scope.messages = messages.data;
+      });
+    }
+
+    $scope.post = function() {
+      FunchatApi.post('Marko(Gofore)', $scope.newMessage).then(listMessages);
+      $scope.newMesasge = undefined;
+    }
+
+    listMessages();
+});
