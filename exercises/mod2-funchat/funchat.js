540f0c22761f66edcf146275bbb871b16e4caa44
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2015-10-20 08:55:49 +0300

    improvements to funchat reference implementation

--- exercises/mod2-funchat/funchat.js
+++ exercises/mod2-funchat/funchat.js
@@ -1,21 +1,32 @@
 angular.module('funtodoApp', [])
   .controller('FunchatCtrl', function($scope, $http, $log, $timeout) {
-    
+
+    $scope.author = "";
+    $scope.messageCounter = 0;
+
+    $scope.clearInput = function () {
+      $scope.author = "";
+      $scope.message = "";
+    };
+
     $scope.addMessage = function (author, message) {
       $http.post('http://funchat-api.herokuapp.com/messages', {
         author: author,
         content: message
       }).success(function (data) {
         $scope.messages.splice(0,0,data);
+        $scope.clearInput();
+        $scope.messageCounter += 1;
       });
     };
 
     $scope.removeMessage = function (id) {
+      $scope.messages = $scope.messages.filter(function (item ) {
+        return item._id !== id;
+      });
+      $scope.note = "Message with id:' + id + ' was deleted successfully";
       $http.delete('http://funchat-api.herokuapp.com/messages/' + id).success(function () {
-        $scope.messages = $scope.messages.filter(function (item ) {
-          return item._id !== id;
-        });
-        $scope.note = "Message with id:' + id + ' was deleted successfully";
+
         $timeout(function () {
           $scope.note = "";
         }, 1000);
